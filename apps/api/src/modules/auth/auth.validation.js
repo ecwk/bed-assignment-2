a35f57b59d228df142360477556e8d527a138f78
Edison Chan <edisonchanwk@gmail.com>
2022-07-21 23:03:56 +0800

    feat(api): remove value injection in auth signup response

--- apps/api/src/modules/auth/auth.validation.js
+++ apps/api/src/modules/auth/auth.validation.js
@@ -10,7 +10,7 @@ const SignupValidationSchema = (database) => {
       .string()
       .test(
         'username-is-unique',
-        '${value} is already taken',
+        'Username is already taken',
         async (value) => {
           const user = await userModel.findOne('username', value);
           return !user;
@@ -20,32 +20,23 @@ const SignupValidationSchema = (database) => {
     email: yup
       .string()
       .email()
-      .test(
-        'email-is-unique',
-        '${value} has already been registered',
-        async (value) => {
-          const user = await userModel.findOne('email', value);
-          return !user;
-        }
-      )
+      .test('email-is-unique', 'Email is already taken', async (value) => {
+        const user = await userModel.findOne('email', value);
+        return !user;
+      })
       .required(),
     contact: yup
       .string()
-      .matches(/^\d{7,15}$/, 'contact must be a valid phone number')
-      .test(
-        'contact-is-unique',
-        '${value} has already been registered',
-        async (value) => {
-          const user = await userModel.findOne('contact', value);
-          return !user;
-        }
-      )
+      .test('contact-is-unique', 'Contact is already taken', async (value) => {
+        const user = await userModel.findOne('contact', value);
+        return !user;
+      })
       .required(),
     password: yup
       .string()
       .matches(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/,
-        'password must contain at least 1 uppercase, lowercase, number and special character (!@#$%^&*)'
+        'Password must contain at least 1 uppercase, lowercase, number and special character (!@#$%^&*)'
       )
       .required(),
     profile_pic_url: yup.string().url().notRequired()
