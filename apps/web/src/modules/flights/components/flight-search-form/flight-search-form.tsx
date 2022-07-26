44724e46d0e632523a0d9245317a341fc61c3f3f
Edison Chan <edisonchanwk@gmail.com>
2022-07-26 13:39:38 +0800

    refactor(web): refactor flight-search-form.tsx

--- apps/web/src/modules/flights/components/flight-search-form/flight-search-form.tsx
+++ apps/web/src/modules/flights/components/flight-search-form/flight-search-form.tsx
@@ -8,10 +8,10 @@ import {
 import { useRouter } from 'next/router';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import { Options } from './options';
 import { SelectDate } from './select-date';
 import { type Airport } from '@common/types';
 import { SelectLocation } from './select-location';
-import { Options } from './options';
 
 export type FlightSearchFormData = {
   from: string; // airportId
@@ -30,23 +30,19 @@ export const FlightSearchForm = ({ airports }: FlightSearchFormProps) => {
   const router = useRouter();
   const methods = useForm<FlightSearchFormData>();
 
-  const colorModeButton = useColorModeValue(
-    'lightModeButton',
-    'darkModeButton'
-  );
-
   const onSubmit = methods.handleSubmit((formData) => {
     let url = '/search/flights?';
-    Object.entries(formData).forEach(([key, value], i) => {
-      if (value instanceof Date) {
-        url += `${key}=${value.toString()}&`;
-      } else {
-        url += `${key}=${value}&`;
-      }
+    Object.entries(formData).forEach(([key, value]) => {
+      url += `${key}=${value}&`;
     });
     url = url.replace(/&$/, '');
     router.push(url);
   });
+
+  const colorModeButton = useColorModeValue(
+    'lightModeButton',
+    'darkModeButton'
+  );
 
   return (
     <FormProvider {...methods}>
@@ -61,15 +57,18 @@ export const FlightSearchForm = ({ airports }: FlightSearchFormProps) => {
         onSubmit={onSubmit}
       >
         <GridItem area="heading" mt={5} mb={10}>
-          <Heading w="100%">Find Your Flight</Heading>
+          <Heading w="100%">Catch Your Flight</Heading>
         </GridItem>
+
         <GridItem area="input" display="flex" flexDir="column" gap={6}>
           <SelectLocation airports={airports} />
           <SelectDate />
         </GridItem>
+
         <GridItem area="options">
           <Options />
         </GridItem>
+
         <GridItem area="button">
           <Button
             type="submit"
