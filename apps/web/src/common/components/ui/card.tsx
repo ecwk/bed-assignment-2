ce7c1815f109535f103542e7dd60b68f8b792e32
Edison Chan <edisonchanwk@gmail.com>
2022-08-05 04:51:00 +0800

    fix(web): fix build

--- apps/web/src/common/components/ui/card.tsx
+++ apps/web/src/common/components/ui/card.tsx
@@ -2,27 +2,27 @@ import { Flex, Img, Text, type FlexProps } from '@chakra-ui/react';
 
 export type CardProps = FlexProps & {};
 
-export const Card = () => {
-  return (
-    <Flex>
-      <Img
-        h="200px"
-        objectFit="contain"
-        borderTopRadius="xl"
-        backgroundColor="brandGray.50"
-        src={src}
-        alt={name}
-      />
-      <Flex
-        flexDir="column"
-        backgroundColor="brandGray.900"
-        borderBottomRadius="xl"
-        p={4}
-      >
-        <Text color="brandGray.50" fontSize="lg" fontWeight="semibold" mr={2}>
-          {name}
-        </Text>
-      </Flex>
-    </Flex>
-  );
-};
+// export const Card = () => {
+//   return (
+//     <Flex>
+//       <Img
+//         h="200px"
+//         objectFit="contain"
+//         borderTopRadius="xl"
+//         backgroundColor="brandGray.50"
+//         src={src}
+//         alt={name}
+//       />
+//       <Flex
+//         flexDir="column"
+//         backgroundColor="brandGray.900"
+//         borderBottomRadius="xl"
+//         p={4}
+//       >
+//         <Text color="brandGray.50" fontSize="lg" fontWeight="semibold" mr={2}>
+//           {name}
+//         </Text>
+//       </Flex>
+//     </Flex>
+//   );
+// };
