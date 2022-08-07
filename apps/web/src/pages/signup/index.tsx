49e494da7a238f2c5639f390aa9c87e7cb6809eb
Edison Chan <edisonchanwk@gmail.com>
2022-08-08 03:28:49 +0800

    feat(web): update signup use main component

--- apps/web/src/pages/signup/index.tsx
+++ apps/web/src/pages/signup/index.tsx
@@ -2,60 +2,59 @@ import { NextPage } from 'next';
 import { Flex, Box, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 
 import { SignupForm } from '@modules/auth';
-import { BackgroundVideo } from '@common/components';
+import { BackgroundVideo, Main } from '@common/components';
 
 const Signup: NextPage = () => {
   return (
-    <Flex
-      as="main"
-      sx={{
-        flexDir: 'column',
-        mt: {
+    <>
+      <Main
+        display="flex"
+        flexDir="column"
+        mt={{
           base: '80px',
           md: '100px'
-        },
-        ml: {
+        }}
+        ml={{
           base: '0',
           lg: '200px'
-        },
-        mb: {
+        }}
+        mb={{
           base: '80px',
           md: '40px'
-        },
-        px: {
+        }}
+        px={{
           base: 5,
           xl: 10
-        },
-        alignItems: {
+        }}
+        alignItems={{
           base: 'center',
           lg: 'flex-start'
-        }
-      }}
-    >
-      <Box as="header">
-        <Heading as="h2" size="md" color="brandGray.50">
-          SIGN UP
-        </Heading>
-        <Heading as="h1" my={4} size="3xl" color="brandGray.50">
-          Create Your Account
-          <Text as="span" color="brandGold.300">
-            .
-          </Text>
-        </Heading>
-      </Box>
-
-      <SignupForm
-        sx={{
-          mt: 6,
-          maxW: {
-            base: '400px',
-            md: '500px'
-          }
         }}
-      />
+      >
+        <Box as="header">
+          <Heading as="h2" size="md" color="brandGray.50">
+            SIGN UP
+          </Heading>
+          <Heading as="h1" my={4} size="3xl" color="brandGray.50">
+            Create Your Account
+            <Text as="span" color="brandGold.300">
+              .
+            </Text>
+          </Heading>
+        </Box>
 
+        <SignupForm
+          sx={{
+            mt: 6,
+            maxW: {
+              base: '400px',
+              md: '500px'
+            }
+          }}
+        />
+      </Main>
       <BackgroundVideo />
-    </Flex>
+    </>
   );
 };
 
